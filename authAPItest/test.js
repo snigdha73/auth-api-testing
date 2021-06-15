a88349da35aa0526a1592dfd34fbe5a326ed25ea
Snigdha <snigdha73.2000@gmail.com>
2021-06-15 12:33:54 +0530

    first

---
+++ authAPItest/test.js
@@ -0,0 +1,52 @@
+// Import the dependencies for testing
+import { json } from 'body-parser';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../auth';
+let expect = chai.expect;
+
+chai.use(require('chai-like'));
+chai.use(require('chai-things'));
+// Configure chai
+chai.use(chaiHttp);
+chai.should();
+describe("Users", () => {
+    describe("GET /", () => {
+        
+        it("should get all users", (done) => {
+            chai.request(app)
+            .get('/')
+            .end((err, res) => {
+                res.should.have.status(200);
+                const arr = res.body;
+                expect(res.body).to.be.an('array');
+                expect(arr[0]).to.be.a('object');
+                
+                done();
+            });
+        });
+        
+        it("should get a single user", (done) => {
+            const id = 'mm@com';
+            chai.request(app)
+            .get(`/${id}`)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('string');
+                res.body.should.include('@');
+                done();
+            });
+        });
+        
+        
+        it("should not get a single user record", (done) => {
+            const id = 'kk@com';
+            chai.request(app)
+            .get(`/Users/get/${id}`)
+            .end((err, res) => {
+                res.should.have.status(404);
+                done();
+            });
+        });
+    });
+});
