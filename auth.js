a2b6b4d1092a849f1b580f545a575fcbe05ce56d
Snigdha <snigdha73.2000@gmail.com>
2021-06-16 16:15:47 +0530

    gitignore

--- auth.js
+++ auth.js
@@ -1,12 +1,9 @@
 import express from 'express';
-import getController from './controllers/getcontrollers';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', getController.getAllUser);
-app.get('/:id', getController.getUserById);
 app.listen(8080);
 
 export default app;
